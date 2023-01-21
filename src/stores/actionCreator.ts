61c17c08ac5078ee79da85e65b80e901018f99b7
Roger Jaffe <rogerjaffe@gmail.com>
2023-01-20 21:19:10 -0800

    Fix types

--- src/stores/actionCreator.ts
+++ src/stores/actionCreator.ts
@@ -1,9 +1,9 @@
 import { action, WritableAtom } from "nanostores";
 
-export function actionCreator<T>(
+export function actionCreator<T, O>(
   atom: WritableAtom<T>,
   name: string,
-  fn: (a: T, b: T) => T
+  fn: (a: T, b?: O) => T
 ): any {
   return action(atom, name, (store, add) => store.set(fn(store.get(), add)));
 }
--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -2,8 +2,8 @@ import { atom } from "nanostores";
 import { actionCreator } from "./actionCreator";
 
 export const counter = atom(0);
-export const counterIncrease = actionCreator<number>(
+export const counterIncrease = actionCreator<number, number>(
   counter,
   "counter/increase",
-  (a, b) => a + b
+  (a, b = 0) => a + b
 );
